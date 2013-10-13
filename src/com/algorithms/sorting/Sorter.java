e73854c343a39348aa9a579370edc2c02a8c2e4f
Alex Vaccarino <agvaccarino@gmail.com>
2013-10-12 21:36:38 -0400

    Initial Commit

---
+++ src/com/algorithms/sorting/Sorter.java
@@ -0,0 +1,17 @@
+package com.algorithms.sorting;
+
+import java.util.Comparator;
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 10/12/13
+ * Time: 9:07 PM.
+ */
+public class Sorter {
+
+    public static int[] countingSort(int[] unsorted) {
+        if (unsorted.length == 0)
+            return unsorted;
+
+    }
+}
